7504fd2d222987ae12f9b0e99d2aa7c2dd731029
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-07-03 01:27:14 -0300

    final js and some css book-list

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bookField = document.querySelector(".book-field");
 
 let editId,
   isEditBook = false,
-  books = JSON.parse(localStorage.getItem("books"));
+  books = JSON.parse(localStorage.getItem("book-list"));
 
 filters.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -27,23 +27,31 @@ function showBook(filter) {
       }
 
       if (filter == book.status || filter == "all") {
-        bookEl += ` <li class="book">
-                     <label for="${id}">
-                     <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                     <p class="${completed}">${book.name}</p>
-                     </label>
-                     <div class="settings">
-                        <ul>
-                            <li onclick='editTask(${id}, "${book.name}")'><i class='bx bx-pencil'></i>Editar</li>
-                            <li onclick='deleteTask(${id}, "${filter}")'><i class='bx bx-x' ></i>Deletar</li>
-                        </ul>
-                     </div>
-                     </li>
-                `;
+        bookEl += `
+        <li class="book">
+          <label class="book-label" for="${id}">
+            <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+            <p class="${completed}">${book.name}</p>
+          </label>
+           <label for="number-${id}" class="number-label">Página:</label>
+      <input type="number" id="number-${id}" class="number-input" value="${
+          book.number || ""
+        }" onchange="saveNumber(${id}, this.value)" onblur="updateNumber(${id}, this.value)">
+    </label>
+          <div class="settings">
+            <ul>
+              <li onclick='editBook(${id}, "${
+          book.name
+        }")'><i class='bx bx-pencil'></i>Editar</li>
+              <li onclick='deleteBook(${id}, "${filter}")'><i class='bx bx-x' ></i>Deletar</li>
+            </ul>
+          </div>
+        </li>
+      `;
       }
     });
   }
-  bookField.innerHTML = bookEl || `<span>Sem leituras disponíveis 😢 </span>`;
+  bookField.innerHTML = bookEl || "<span>Sem leituras disponíveis 😢 </span>";
   let checkBook = bookField.querySelectorAll(".book");
   !checkBook.length
     ? clearAll.classList.remove("active")
@@ -52,4 +60,65 @@ function showBook(filter) {
     ? bookField.classList.add("overflow")
     : bookField.classList.remove("overflow");
 }
-showBook('all');
+showBook("all");
+
+function updateStatus(selectedBook) {
+  let bookName = selectedBook.parentElement.lastElementChild;
+  if (selectedBook.checked) {
+    bookName.classList.add("checked");
+    books[selectedBook.id].status = "completed";
+  } else {
+    bookName.classList.remove("checked");
+    books[selectedBook.id].status = "reading";
+  }
+  localStorage.setItem("book-list", JSON.stringify(books));
+}
+
+function editBook(bookId, textName) {
+  editId = bookId;
+  isEditBook = true;
+  mainInput.value = textName;
+  mainInput.focus();
+  mainInput.classList.add("active");
+}
+
+function deleteBook(deleteId, filter) {
+  isEditBook = false;
+  books.splice(deleteId, 1);
+  localStorage.setItem("book-list", JSON.stringify(books));
+  showBook(filter);
+}
+
+clearAll.addEventListener("click", () => {
+  isEditBook = false;
+  books.splice(0, books.length);
+  localStorage.setItem("book-list", JSON.stringify(books));
+  showBook();
+});
+
+mainInput.addEventListener("keyup", (e) => {
+  let userBook = mainInput.value.trim();
+  if (e.key == "Enter" && userBook) {
+    if (!isEditBook) {
+      books = !books ? [] : books;
+      let bookInfo = { name: userBook, status: "reading" };
+      books.push(bookInfo);
+    } else {
+      isEditBook = false;
+      books[editId].name = userBook;
+    }
+    mainInput.value = "";
+    localStorage.setItem("book-list", JSON.stringify(books));
+    showBook(document.querySelector("span.active").id);
+  }
+});
+
+function saveNumber(bookId, number) {
+  books[bookId].number = number;
+  localStorage.setItem("book-list", JSON.stringify(books));
+}
+
+function updateNumber(bookId, number) {
+  books[bookId].number = number;
+  localStorage.setItem("book-list", JSON.stringify(books));
+}
